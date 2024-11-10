b7862a274612b528feb97729db0b37051fddfe18
Jack Norris <hamuelxd@gmail.com>
2024-11-10 20:22:34 +0800

    Fixed initial commit

--- composeApp/src/commonMain/kotlin/com/thebrownfoxx/unfocus/Greeting.kt
+++
@@ -1,9 +0,0 @@
-package com.thebrownfoxx.unfocus
-
-class Greeting {
-    private val platform = getPlatform()
-
-    fun greet(): String {
-        return "Hello, ${platform.name}!"
-    }
-}
--- composeApp/src/commonMain/kotlin/com/thebrownfoxx/unfocus/Platform.kt
+++
@@ -1,7 +0,0 @@
-package com.thebrownfoxx.unfocus
-
-interface Platform {
-    val name: String
-}
-
-expect fun getPlatform(): Platform
