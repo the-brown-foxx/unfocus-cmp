bc6803d9e853d3539d2872dab7fa173257dc989b
Jack Norris <hamuelxd@gmail.com>
2024-11-13 01:12:06 +0800

    Changed test phase durations to be quicker

--- domain/src/commonMain/kotlin/com/thebrownfoxx/unfocus/domain/TestPhaseDurationProvider.kt
+++ domain/src/commonMain/kotlin/com/thebrownfoxx/unfocus/domain/TestPhaseDurationProvider.kt
@@ -1,15 +1,14 @@
 package com.thebrownfoxx.unfocus.domain
 
 import kotlin.time.Duration
-import kotlin.time.Duration.Companion.minutes
 import kotlin.time.Duration.Companion.seconds
 
 object TestPhaseDurationProvider : PhaseDurationProvider {
     override val Phase.duration: Duration
         get() = when (this) {
-            Phase.Focus -> 10.seconds
-            Phase.EyeBreak -> 3.seconds
-            Phase.SitBreak -> 5.seconds
-            Phase.FullRest -> 1.minutes
+            Phase.Focus -> 5.seconds
+            Phase.EyeBreak -> 2.seconds
+            Phase.SitBreak -> 3.seconds
+            Phase.FullRest -> 10.seconds
         }
 }
